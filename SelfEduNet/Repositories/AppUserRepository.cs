ab4eac7a2f0c055f6be57dd6de0a683bc6e4473a
Dmytro Khoma <dkhoma@enmark.com>
2025-04-08 01:37:04 +0300

    User repository

---
+++ SelfEduNet/Repositories/AppUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SelfEduNet.Models;
+
+namespace SelfEduNet.Repositories
+{
+	public interface IAppUserRepository
+	{
+
+	}
+	public class AppUserRepository(UserManager<AppUser> userManager)
+	{
+		private readonly UserManager<AppUser> _userManager = userManager;
+
+	
+	}
+}
