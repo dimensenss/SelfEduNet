34f045c96dcafb85af1178050aec367dfa9c44bd
Dmytro Khoma <dkhoma@enmark.com>
2025-04-08 01:13:46 +0300

    Admin templates, controllers, services

--- SelfEduNet/Areas/Admin/Helpers/YouTubeHelper.cs
+++ SelfEduNet/Areas/Admin/Helpers/YouTubeHelper.cs
@@ -15,4 +15,23 @@ public static class YouTubeHelper
         var videoId = GetYouTubeVideoId(youtubeUrl);
         return videoId != null ? $"https://www.youtube.com/embed/{videoId}" : null;
     }
+    public static async Task<string> GetYouTubeVideoTitleAsync(string youtubeUrl)
+    {
+	    var videoId = GetYouTubeVideoId(youtubeUrl);
+	    if (videoId == null)
+		    return null;
+
+	    using var httpClient = new HttpClient();
+	    httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0"); // Эмулируем браузер
+	    var response = await httpClient.GetStringAsync($"https://www.youtube.com/watch?v={videoId}");
+
+	    var match = Regex.Match(response, @"<title>(.*?)<\/title>");
+	    if (match.Success)
+	    {
+		    var title = match.Groups[1].Value;
+		    return title.Replace(" - YouTube", "").Trim();
+	    }
+
+	    return "Video not found";
+    }
 }
