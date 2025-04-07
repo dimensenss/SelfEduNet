e01dfc9881c15c46281578e3b7f23655df7426cb
Dmytro Khoma <dkhoma@enmark.com>
2025-04-08 01:25:06 +0300

    Cloudinary PhotoService

--- SelfEduNet/Services/PhotoService.cs
+++ SelfEduNet/Services/PhotoService.cs
@@ -3,16 +3,21 @@ using CloudinaryDotNet.Actions;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using EduProject.Helpers;
+using System.Text.RegularExpressions;
 
 namespace EduProject.Services
 {
 
-    public interface IPhotoService
-    {
-        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
-        Task<DeletionResult> DeletePhotoAsync(string publicId);
-    }
-	public class PhotoService: IPhotoService
+	public interface IPhotoService
+	{
+		Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+		Task<VideoUploadResult> AddVideoAsync(IFormFile file);
+
+		Task<DeletionResult> DeleteFileAsync(string publicId);
+		List<string> ExtractImageUrls(string htmlContent);
+
+	}
+	public class PhotoService : IPhotoService
 	{
 		private readonly Cloudinary _cloudinary;
 
@@ -30,7 +35,7 @@ namespace EduProject.Services
 			var uploadResult = new ImageUploadResult();
 			if (file.Length > 0)
 			{
-				using (var stream = file.OpenReadStream())
+				await using(var stream = file.OpenReadStream())
 				{
 					var uploadParams = new ImageUploadParams
 					{
@@ -43,11 +48,54 @@ namespace EduProject.Services
 			return uploadResult;
 		}
 
-		public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+		public async Task<VideoUploadResult> AddVideoAsync(IFormFile file)
+		{
+			var uploadResult = new VideoUploadResult();
+			if (file.Length > 0)
+			{
+				await using (var stream = file.OpenReadStream())
+				{
+					var uploadParams = new VideoUploadParams()
+					{
+						File = new FileDescription(file.FileName, stream),
+						Transformation = new Transformation()
+							.Width(1280).Height(720).Crop("limit")
+							.BitRate("800k")
+							.Quality("auto:good")
+					};
+					uploadResult = await _cloudinary.UploadAsync(uploadParams);
+				}
+			}
+			return uploadResult;
+		}
+
+		public async Task<DeletionResult> DeleteFileAsync(string publicId)
 		{
 			var deleteParams = new DeletionParams(publicId);
 			var result = await _cloudinary.DestroyAsync(deleteParams);
 			return result;
+		}
+		public List<string> ExtractImageUrls(string htmlContent)
+		{
+			var imageUrls = new List<string>();
+
+			var htmlRegex = new Regex("<img[^>]+src=[\"']([^\"']+)[\"']", RegexOptions.IgnoreCase);
+			var htmlMatches = htmlRegex.Matches(htmlContent);
+
+			var markdownRegex = new Regex(@"!\[\]\((.*?)\)", RegexOptions.IgnoreCase);
+			var markdownMatches = markdownRegex.Matches(htmlContent);
+
+			foreach (Match match in htmlMatches)
+			{
+				imageUrls.Add(match.Groups[1].Value);
+			}
+
+			foreach (Match match in markdownMatches)
+			{
+				imageUrls.Add(match.Groups[1].Value);
+			}
+
+			return imageUrls;
 		}
 	}
 }
