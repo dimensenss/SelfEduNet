89dd163b0a903b674efcb7ed707063d785f5f0d4
Dmytro Khoma <dkhoma@enmark.com>
2025-04-08 01:33:34 +0300

    Course service

--- SelfEduNet/Repositories/CourseRepository.cs
+++ SelfEduNet/Repositories/CourseRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using SelfEduNet.Data;
 using SelfEduNet.Models;
 using System.Linq;
+using SelfEduNet.ViewModels;
 
 namespace SelfEduNet.Repositories
 {
@@ -12,27 +13,36 @@ namespace SelfEduNet.Repositories
 	    public IQueryable<Course> GetAllCoursesQuery();
 	    public IQueryable<Course> GetAllCoursesByOwnerQuery(string userId);
         public Task<IEnumerable<Course>> GetCoursesByCategoryAsync(int catId);
-        public Task<Course> GetCourseByIdAsync(int courseId);
-        public Task<Course> GetCourseWithInfoByIdAsync(int courseId);
+        public Task<CourseModules> GetModuleByIdAsync(int moduleId);
+        public Task<IEnumerable<CourseModules>> GetAllModulesByCourseId(int courseId);
+        public Task<Course> GetCourseWithModulesByIdAsync(int courseId);
+		public Task<Course?> GetCourseByIdAsync(int courseId);
+		public Task<Lesson?> GetLessonByIdAsync(int? lessonId, string? userId);
+		public Task<Lesson?> GetNextLessonAsync(int courseId, int lessonId);
+		public LessonStatistics GetLessonStatisticsByIdAsync(List<Step> userSteps);
+		public Task<Course?> GetCourseWithInfoByIdAsync(int courseId);
         public Task<bool> IsCourseOwnedByUser(int courseId, string userId);
         public Task<List<AppUser>> GetCourseAuthorsAsync(int courseId);
         public Task<IEnumerable<Course>> GetCoursesByOwnerAsync(string userId);
-
+        public Task<bool> AddModuleAsync(CourseModules module);
+        public Task<bool> AddLessonAsync(Lesson lesson);
+        public Task<bool> AddAsync(Course course);
+		public bool Add(Course course);
         public bool Update(Course course);
+        public Task<bool> UpdateModuleAsync(CourseModules module);
+        public Task<bool> UpdateLessonAsync(Lesson lesson);
+        public Task<int> GetMaxLessonOrderAsync(int moduleId);
+        public Task<int> GetMaxModuleOrderAsync(int courseId);
 		public bool Delete(Course course);
+		public bool DeleteModule(CourseModules module);
+		public bool DeleteLesson(Lesson lesson);
         public bool Save();
-
-
     }
-    public class CourseRepository: ICourseRepository
+    public class CourseRepository(ApplicationDbContext context) : ICourseRepository
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context = context;
 
-        public CourseRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+		public async Task<IEnumerable<Course>> GetAllCoursesAsync()
         {
             return await _context.Courses
 	            .Where(c => c.IsPublished == true)
@@ -44,7 +54,8 @@ namespace SelfEduNet.Repositories
         public async Task<IEnumerable<Course>> GetCoursesByCategoryAsync(int catId)
         {
 	        return await _context.Courses
-		        .Where(c => c.IsPublished == true && c.CategoryId == catId)
+		        .Where(c => c.IsPublished == true 
+		                    && c.CategoryId == catId)
 				.Include(c => c.Owner)
 		        .OrderBy(c => c.CreatedAt)
 		        .ToListAsync();
@@ -52,31 +63,133 @@ namespace SelfEduNet.Repositories
 
 		public IQueryable<Course> GetAllCoursesQuery()
         {
-            return _context.Courses.AsQueryable();
-        }
+			return _context.Courses
+				.Include(c => c.Owner)
+				.Include(c => c.Info)
+				.ThenInclude(ci => ci.Authors) 
+				.AsQueryable();
+		}
 
         public IQueryable<Course> GetAllCoursesByOwnerQuery(string userId)
         {
-			return _context.Courses
-				.Where(c => c.OwnerId.Contains(userId))
-				.ToList()
-				.AsQueryable();
-        }
+	        return _context.Courses
+		        .Where(c => c.OwnerId.Contains(userId));
 
-		public async Task<Course> GetCourseByIdAsync(int courseId)
+        }
+        public async Task<CourseModules> GetModuleByIdAsync(int moduleId)
         {
-            return await _context.Courses.FirstOrDefaultAsync(c => c.Id == courseId);
+            return await _context.CourseModules.FirstOrDefaultAsync(c => c.Id == moduleId);
         }
-
-        public async Task<Course> GetCourseWithInfoByIdAsync(int courseId)
+        public async Task<IEnumerable<CourseModules>> GetAllModulesByCourseId(int courseId)
+        {
+	        return await _context.CourseModules
+		        .Where(c => c.CourseId == courseId)
+		        .ToListAsync();
+		}
+		public async Task<Course?> GetCourseByIdAsync(int courseId)
         {
             return await _context.Courses
-                .Include(c => c.Info)
-                .ThenInclude(a => a.Authors)
-                .FirstOrDefaultAsync(c => c.Id == courseId);
+	            .Include(c=>c.Info)
+	            .Include(c=> c.Modules)
+	            .ThenInclude(m=>m.Lessons)
+	            .FirstOrDefaultAsync(c => c.Id == courseId);
+        }
+        public async Task<Lesson?> GetLessonByIdAsync(int? lessonId, string? userId)
+        {
+			if (lessonId == null)
+				return null;
+
+			var query = context.Lessons
+				.AsQueryable();
+
+			if (userId != null)
+			{
+				query = query
+					.Include(l => l.UserLessons.Where(ul => ul.UserId == userId));
+			}
+
+			var lesson = await query
+				.FirstOrDefaultAsync(l => l.Id == lessonId);
+
+			return lesson;
+		}
+
+		public async Task<Lesson?> GetNextLessonAsync(int courseId, int lessonId)
+		{
+			var lessons = await _context.Lessons
+				.Include(l => l.CourseModule)
+				.Where(l => l.CourseModule!.CourseId == courseId)
+				.OrderBy(l => l.CourseModule!.Order)
+				.ThenBy(l => l.Order)
+				.ToListAsync();
+
+			var currentIndex = lessons.FindIndex(l => l.Id == lessonId);
+
+			if (currentIndex == -1 || currentIndex + 1 >= lessons.Count)
+				return null;
+
+			return lessons[currentIndex + 1];
+		}
+
+		public LessonStatistics GetLessonStatisticsByIdAsync(List<Step> steps)
+        {
+	        var userSteps = steps.SelectMany(s => s.UserSteps).ToList();
+			if (!userSteps.Any())
+	        {
+		        return new LessonStatistics
+		        {
+			        StartLessonTime = DateTime.MinValue,
+			        EndLessonTime = DateTime.MinValue,
+			        CompletedStepsCount = 0,
+			        CompletedStepsPercentage = 0
+		        };
+	        }
+	        var completedSteps = userSteps.Where(us => us.IsCompleted).ToList();
+	        int totalSteps = userSteps.Count;
+	        int completedStepsCount = completedSteps.Count;
+	        int completedStepsPercentage = totalSteps > 0 ? (completedStepsCount * 100 / totalSteps) : 0;
+
+	        return new LessonStatistics
+	        {
+		        StartLessonTime = userSteps.Min(us => us.ViewedAt) ?? DateTime.MinValue,
+		        EndLessonTime = completedSteps.Any()
+			        ? completedSteps.Max(us => us.CompletedAt) ?? DateTime.MinValue
+			        : DateTime.MinValue,
+		        CompletedStepsCount = completedStepsCount,
+		        CompletedStepsPercentage = completedStepsPercentage
+	        };
         }
 
-        public async Task<IEnumerable<Course>> GetCoursesByOwnerAsync(string userId)
+        public async Task<Course?> GetCourseWithInfoByIdAsync(int courseId)
+        {
+			return await _context.Courses
+				.Include(c => c.Info)
+				.ThenInclude(a => a.Authors)
+				.Include(c => c.Modules)  // Загружаем связанные модули
+				.ThenInclude(m => m.Lessons)  // Загружаем уроки в этих модулях
+				.FirstOrDefaultAsync(c => c.Id == courseId);
+		}
+        public async Task<Course> GetCourseWithModulesByIdAsync(int courseId)
+        {
+	        var course = await _context.Courses
+		        .Include(c => c.Modules)
+		        .ThenInclude(m => m.Lessons)
+		        .FirstOrDefaultAsync(c => c.Id == courseId);
+
+	        if (course != null)
+	        {
+		        course.Modules = course.Modules.OrderBy(m => m.Order).ToList();
+
+		        foreach (var module in course.Modules)
+		        {
+			        module.Lessons = module.Lessons.OrderBy(l => l.Title).ToList();
+		        }
+	        }
+
+	        return course;
+        }
+
+		public async Task<IEnumerable<Course>> GetCoursesByOwnerAsync(string userId)
         {
             return await _context.Courses
                 .Where(c => c.OwnerId.Contains(userId))
@@ -97,10 +210,75 @@ namespace SelfEduNet.Repositories
 	            .ToListAsync();
         }
 
+        public async Task<bool> AddModuleAsync(CourseModules module)
+        {
+            await _context.CourseModules.AddAsync(module);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> AddLessonAsync(Lesson lesson)
+        {
+			await _context.Lessons.AddAsync(lesson);
+			return await SaveAsync();
+		}
+
+        public async Task<bool> AddAsync(Course course)
+		{
+			await _context.Courses.AddAsync(course);
+			return await SaveAsync();
+		}
+
+		public bool Add(Course course)
+        {
+	        _context.Courses.Add(course);
+	        return Save();
+        }
+
+        public async Task<int> GetMaxLessonOrderAsync(int moduleId)
+        {
+	        var maxOrder = await _context.Lessons
+		        .Where(l => l.CourseModuleId == moduleId)
+		        .Select(s => (int?)s.Order)
+		        .MaxAsync();
+
+	        return maxOrder ?? 0;
+		}
+
+        public async Task<int> GetMaxModuleOrderAsync(int courseId)
+        {
+			var maxOrder = await _context.CourseModules
+				.Where(m => m.CourseId == courseId)
+				.Select(m => (int?)m.Order)
+				.MaxAsync();
+
+			return maxOrder ?? 0;
+		}
+
         public bool Delete(Course course)
         {
 	        _context.Courses.Remove(course);
 	        return Save();
+        }
+        public bool DeleteModule(CourseModules module)
+        {
+	        _context.CourseModules.Remove(module);
+	        return Save();
+        }
+		public bool DeleteLesson(Lesson lesson)
+        {
+            _context.Lessons.Remove(lesson);
+            return Save();
+		}
+
+		public async Task<bool> UpdateModuleAsync(CourseModules module)
+        {
+	        _context.Update(module);
+			return await SaveAsync();
+		}
+        public async Task<bool> UpdateLessonAsync(Lesson lesson)
+        {
+	        _context.Update(lesson);
+	        return await SaveAsync();
         }
 		public bool Update(Course course)
         {
@@ -113,5 +291,10 @@ namespace SelfEduNet.Repositories
             var saved = _context.SaveChanges();
             return saved > 0 ? true : false;
         }
-    }
+
+        public async Task<bool> SaveAsync()
+        {
+			return await _context.SaveChangesAsync() > 0;
+		}
+	}
 }
---
+++ SelfEduNet/Services/CourseService.cs
@@ -0,0 +1,338 @@
+﻿using CloudinaryDotNet;
+using EduProject.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SelfEduNet.Data;
+using SelfEduNet.Data.Enum;
+using SelfEduNet.Models;
+using SelfEduNet.Repositories;
+using SelfEduNet.ViewModels;
+
+namespace SelfEduNet.Services
+{
+    public interface ICourseService
+	{
+		Task<EditCourseViewModel> GetEditViewModelAsync(int id);
+		Task<bool> UpdateCourseAsync(int id, EditCourseViewModel courseVM);
+		Task<bool> UpdateModuleAsync(CourseModules module);
+		Task<bool> UpdateLessonAsync(Lesson lesson);
+		Task<bool> CreateCourseAsync(CreateCourseViewModel courseVM);
+		Task<bool> CreateModuleAsync(int courseId);
+		Task<bool> CreateLessonAsync(int moduleId, string title);
+		Task<Lesson> GetLessonByIdAsync(int? lessonId, string userId = null);
+		Task<Lesson?> GetNextLessonAsync(int courseId, int lessonId);
+		Task<LessonStatistics> GetLessonStatisticsByIdAsync(int lessonId, string userId);
+		Task<CourseContentViewModel> GetCourseContentViewModelAsync(int id);
+		Task<IEnumerable<Course>> GetAllCoursesAsync();
+		IQueryable<Course> GetAllCoursesByOwnerQuery(string userId);
+		Task<IEnumerable<Course>> GetCoursesByOwnerAsync(string userId);
+		Task<CourseModules> GetModuleByIdAsync(int moduleId);
+		Task<bool> UpdateModuleOrderAsync(List<ModuleOrderViewModel> moduleOrder);
+		Task<IEnumerable<CourseModules>> GetAllModulesByCourseId(int courseId);
+		Task<Course> GetCourseByIdAsync(int id);
+		Task<Course> GetCourseWithInfoByIdAsync(int id);
+		public Task<int> GetMaxLessonOrderAsync(int moduleId);
+		public Task<int> GetMaxModuleOrderAsync(int courseId);
+		public bool DeleteCourse(Course course);
+		public bool DeleteModule(CourseModules module);
+		public bool DeleteLesson(Lesson lesson);
+		public bool Update(Course course);
+	}
+	public class CourseService(ApplicationDbContext context, ICourseRepository courseRepository, ICategoryService categoryService, IStepRepository stepRepository,
+		IPhotoService photoService): ICourseService
+	{
+		private readonly ApplicationDbContext _context = context;
+		private readonly ICourseRepository _courseRepository = courseRepository;
+		private readonly ICategoryService _categoryService = categoryService;
+		private readonly IStepRepository _stepRepository = stepRepository;
+		private readonly IPhotoService _photoService = photoService;
+
+		public async Task<EditCourseViewModel> GetEditViewModelAsync(int id)
+		{
+			var course = await _courseRepository.GetCourseWithInfoByIdAsync(id);
+			if (course == null) return null;
+
+			var courseVM = new EditCourseViewModel
+			{
+				Id = course.Id,
+				PreviewURL = course.Preview,
+				CourseName = course.CourseName,
+				Description = course.Description,
+				PromoText = course.PromoText,
+				Language = course.Language,
+				Difficulty = course.Difficulty,
+				FullPrice = course.FullPrice,
+				HaveCertificate = course.HaveCertificate,
+				Workload = course.Info.Workload,
+				Authors = course.Info.Authors,
+				Modules = course.Modules,
+				PreviewVideo = course.Info.PreviewVideo,
+				IsPublished = course.IsPublished
+			};
+			
+			return courseVM;
+		}
+
+		public async Task<Lesson?> GetNextLessonAsync(int courseId, int lessonId)
+		{
+			return await _courseRepository.GetNextLessonAsync(courseId, lessonId);
+		}
+
+		public async Task<LessonStatistics> GetLessonStatisticsByIdAsync(int lessonId, string userId)
+		{
+			var steps = await _stepRepository.GetStepsByLessonIdAsync(lessonId, userId);
+			return _courseRepository.GetLessonStatisticsByIdAsync(steps);
+		}
+
+		public async Task<CourseContentViewModel> GetCourseContentViewModelAsync(int id)
+		{
+			var course = await _courseRepository.GetCourseWithModulesByIdAsync(id);
+			if (course == null) return null;
+
+			var courseVM = new CourseContentViewModel
+			{
+				Id = course.Id,
+				CourseName = course.CourseName,
+				Modules = course.Modules,
+				Lessons = course.Modules
+					.SelectMany(module => module.Lessons ?? new List<Lesson>()) // Flatten lessons from all modules
+					.ToList()
+			};
+
+			return courseVM;
+		}
+
+		public IQueryable<Course> GetAllCoursesByOwnerQuery(string userId)
+		{
+			return _courseRepository.GetAllCoursesByOwnerQuery(userId);
+		}
+
+		public async Task<bool> UpdateCourseAsync(int id, EditCourseViewModel courseVM)
+		{
+			await using var transaction = await _context.Database.BeginTransactionAsync();
+			{
+				try
+				{
+					var course = await _courseRepository.GetCourseWithInfoByIdAsync(id);
+					if (course == null) return false;
+
+					string previewURL = courseVM.PreviewURL;
+					if (courseVM.Preview != null)
+					{
+						try
+						{
+							var fileInfo = new FileInfo(course.Preview);
+							string publicId = Path.GetFileNameWithoutExtension(fileInfo.Name);
+							await _photoService.DeleteFileAsync(publicId);
+						}
+						catch
+						{
+							//TODO : log error
+						}
+
+						var resultPhoto = await _photoService.AddPhotoAsync(courseVM.Preview);
+						previewURL = resultPhoto.Url.ToString();
+					}
+
+					course.Info.Workload = courseVM.Workload;
+					course.Info.PreviewVideo = courseVM.PreviewVideo;
+
+					course.Preview = previewURL;
+					course.CourseName = courseVM.CourseName;
+					course.Description = courseVM.Description;
+					course.Language = courseVM.Language;
+					course.Difficulty = courseVM.Difficulty;
+					course.FullPrice = courseVM.FullPrice;
+					course.HaveCertificate = courseVM.HaveCertificate;
+					course.IsPublished = courseVM.IsPublished;
+
+					return _courseRepository.Update(course);
+					await transaction.CommitAsync();
+
+				}
+				catch (Exception ex)
+				{
+					await transaction.RollbackAsync();
+					return false;
+				}
+			}
+		}
+
+		public async Task<bool> CreateCourseAsync(CreateCourseViewModel courseVM)
+		{
+			var course = new Course
+			{
+				OwnerId = courseVM.OwnerId,
+				Owner = courseVM.Owner,
+				CourseName = courseVM.CourseName
+			};
+
+			var draftsCategory = await _categoryService.GetOrCreateDraftsCategoryAsync();
+			course.Category = draftsCategory;
+
+			return await courseRepository.AddAsync(course);
+		}
+
+		public async Task<bool> UpdateModuleAsync(CourseModules module)
+		{
+			return await courseRepository.UpdateModuleAsync(module);
+		}
+		public async Task<bool> UpdateLessonAsync(Lesson lesson)
+		{
+			return await courseRepository.UpdateLessonAsync(lesson);
+		}
+
+		private async Task<string> GenerateNewModuleTitle(int courseId)
+		{
+			var existingModules = await _courseRepository.GetAllModulesByCourseId(courseId);
+			var existingTitles = existingModules.Select(m => m.Title);
+
+			int moduleNumber = 1;
+			while (existingTitles.Contains($"Новий модуль {moduleNumber}"))
+			{
+				moduleNumber++;
+			}
+
+			return $"Новий модуль {moduleNumber}";
+		}
+
+		public async Task<bool> CreateModuleAsync(int courseId)
+		{
+			string title = await GenerateNewModuleTitle(courseId);
+			int maxOrder = await GetMaxModuleOrderAsync(courseId);
+			var module = new CourseModules
+            {
+				Title = title,
+                CourseId = courseId,
+				Order = maxOrder + 1
+            };
+            return await _courseRepository.AddModuleAsync(module);
+        }
+		public async Task<int> GetMaxLessonOrderAsync(int moduleId)
+		{
+			return await _courseRepository.GetMaxLessonOrderAsync(moduleId);
+		}
+		public async Task<int> GetMaxModuleOrderAsync(int courseId)
+		{
+			return await _courseRepository.GetMaxModuleOrderAsync(courseId);
+		}
+		public async Task<bool> CreateLessonAsync(int moduleId, string title)
+		{
+			await using var transaction = await _context.Database.BeginTransactionAsync();
+			{
+				try
+				{
+					int maxOrder = await _courseRepository.GetMaxLessonOrderAsync(moduleId);
+					var lesson = new Lesson
+					{
+						CourseModuleId = moduleId,
+						Title = title,
+						Order = maxOrder + 1
+					};
+
+					bool lessonSaved = await _courseRepository.AddLessonAsync(lesson);
+					if (!lessonSaved)
+					{
+						await transaction.RollbackAsync();
+						return false;
+					}
+
+					var firstStep = new Step
+					{
+						LessonId = lesson.Id,
+						Content = "Урок створений системою",
+						StepType = StepType.Text,
+						Order = 1
+					};
+
+					bool stepSaved = await _stepRepository.AddAsync(firstStep);
+					if (!stepSaved)
+					{
+						await transaction.RollbackAsync();
+						return false;
+					}
+
+					await transaction.CommitAsync();
+					return true;
+				}
+				catch (Exception ex)
+				{
+					await transaction.RollbackAsync();
+					return false;
+				}
+			}
+		}
+
+		public async Task<Lesson> GetLessonByIdAsync(int? lessonId, string userId = null)
+		{
+			return await _courseRepository.GetLessonByIdAsync(lessonId, userId);
+		}
+
+		public async Task<CourseModules> GetModuleByIdAsync(int moduleId)
+        {
+            return await _courseRepository.GetModuleByIdAsync(moduleId);
+        }
+
+        public async Task<IEnumerable<CourseModules>> GetAllModulesByCourseId(int courseId)
+        {
+	        return await _courseRepository.GetAllModulesByCourseId(courseId);
+		}
+        public async Task<bool> UpdateModuleOrderAsync(List<ModuleOrderViewModel> moduleOrder)
+        {
+	        foreach (var module in moduleOrder)
+	        {
+		        var existingModule = await _courseRepository.GetModuleByIdAsync(module.ModuleId);
+		        if (existingModule != null)
+		        {
+			        existingModule.Order = module.Order;
+			        await _courseRepository.UpdateModuleAsync(existingModule);
+		        }
+		        else
+		        {
+			        return false;
+		        }
+	        }
+
+	        return true;
+        }
+
+		public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+		{
+			return await _courseRepository.GetAllCoursesAsync();
+		}
+
+		public async Task<IEnumerable<Course>> GetCoursesByOwnerAsync(string userId)
+		{
+			return await _courseRepository.GetCoursesByOwnerAsync(userId);
+		}
+        
+
+        public async Task<Course> GetCourseByIdAsync(int id)
+		{
+			return await _courseRepository.GetCourseByIdAsync(id);
+		}
+
+		public async Task<Course> GetCourseWithInfoByIdAsync(int id)
+		{
+			return await _courseRepository.GetCourseWithInfoByIdAsync(id);
+		}
+
+		public bool DeleteCourse(Course course)
+		{
+			return _courseRepository.Delete(course);
+		}
+		public bool DeleteModule(CourseModules module)
+		{
+			return _courseRepository.DeleteModule(module);
+		}
+		public bool DeleteLesson(Lesson lesson)
+		{
+			return _courseRepository.DeleteLesson(lesson);
+		}
+
+		public bool Update(Course course)
+		{
+			return _courseRepository.Update(course);
+		}
+	}
+}
